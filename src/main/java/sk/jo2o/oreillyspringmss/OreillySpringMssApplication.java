81c9528af5a94d8749831294e0bdfd814ed98bb4
JozefMikus <jozef.mikus@kyndryl.com>
2022-09-18 09:20:13 +0200

    -- init

---
+++ src/main/java/sk/jo2o/oreillyspringmss/OreillySpringMssApplication.java
@@ -0,0 +1,13 @@
+package sk.jo2o.oreillyspringmss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OreillySpringMssApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OreillySpringMssApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sk/jo2o/oreillyspringmss/OreillySpringMssApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.jo2o.oreillyspringmss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OreillySpringMssApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
